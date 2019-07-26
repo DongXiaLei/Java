063a119d88196c94a183e2a639ab315761e45525
DongXiaLei <1594003819@qq.com>
2019-07-26 12:54:00 +0800

    LC76

--- src/LeetCode/LC76.java
+++ src/LeetCode/LC76.java
@@ -6,48 +6,78 @@ public class LC76 {
     public static String minWindow(String s, String t) {
         char[] S = s.toCharArray();
         char[] T = t.toCharArray();
-        Map<Character, Queue<Integer>> map = new HashMap<>();
-        for(int i = 0;i<S.length;i++){
-            for(int j =0 ;j<T.length;j++){
-                if(S[i] == T[j]){
-                    if(map.containsKey(T[j])){
-                        map.get(T[j]).add(i);
+        if(s.length()<t.length())return ""; // s<t
+
+        Map<Character,Integer> mapT = new HashMap<>();
+        mapT = countNums(T,0,T.length-1);
+        int left = 0,right =0,ans=S.length,ansLeft =-1,ansRight=-1;
+        Map<Character,Integer> tmpMap = countNums(S,left,right);
+        while (left<S.length){
+            if(isMatch(mapT,tmpMap)){
+                if((right-left)<ans){
+                    ans = right-left;
+                    ansLeft = left;
+                    ansRight = right;
+                }
+                char c =S[left];//将左边的删除
+                left++;
+                if(tmpMap.get(c)>1){
+                    int tmp = tmpMap.get(c);
+                    tmpMap.put(c,--tmp);
+                }else {
+                    tmpMap.remove(c);
+                }
+            }else {
+                if(right<S.length-1) {
+                    right++;
+                    char c = S[right];//将右边的添加
+                    if(tmpMap.containsKey(c)){
+                        int tmp = tmpMap.get(c);
+                        tmpMap.put(c,++tmp);
                     }else {
-                        Queue<Integer> queuenew = new LinkedList<>();
-                        queuenew.add(i);
-                        map.put(T[j],queuenew);
+                        tmpMap.put(c,1);
                     }
-                    break;
+
+                }else {
+                    if(!isMatch(mapT,tmpMap)){
+                        break;
+                    }
                 }
             }
         }
-        int ans = S.length,ansmin =0 ,ansmax =0 ,minIndex = S.length,maxIndex =-1,delIndex =0;
-        while (true){
-            minIndex = S.length;maxIndex =-1;
-            for(int i =0;i<T.length;i++){
-                Queue<Integer> queue = map.get(T[i]);
-                if(queue!=null && queue.peek()<minIndex) {
-                    minIndex = queue.peek();
-                    delIndex = i;
-                }
-                if(queue!=null && queue.peek()>maxIndex) maxIndex = queue.peek();
-
+        if(ansLeft!= -1 && ansRight!=-1){
+            return new String(Arrays.copyOfRange(S,ansLeft,ansRight+1));
+        }else return "";
+    }
+    public static Map<Character,Integer> countNums(char[] nums, int begin,int end){
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i =begin;i<=end;i++){
+            if(map.containsKey(nums[i])){
+                int tmp = map.get(nums[i]);
+                map.put(nums[i],++tmp);
             }
-            if((maxIndex-minIndex)<ans) {
-                ans =maxIndex -minIndex;
-                ansmin = minIndex;
-                ansmax = maxIndex;
+            else{
+                map.put(nums[i],1);
             }
-            map.get(T[delIndex]).poll();
-            break;
         }
-
-        return null;
+        return map;
+    }
+    public static boolean isMatch(Map<Character,Integer> mapT, Map<Character,Integer> mapS){
+        boolean flag = true;
+        for(char ele:mapT.keySet()){
+            if(mapS.containsKey(ele) && mapS.get(ele)>=mapT.get(ele)){
+                flag =true;
+            }else {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
     }
     public static void main(String[] args){
-        String s = "ADOBECODEBANC";
-        String t = "ABC";
-        minWindow(s,t);
+        String s = "ABC";
+        String t = "CBA";
+        String ss= minWindow(s,t);
     }
 
 
